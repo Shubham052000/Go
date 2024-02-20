0a32efa634fc703717473c4918a341ea10a3fc54
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2024-02-20 10:51:48 +0530

    writing tests with go and testing the function and io

---
+++ course/02_cards/deck_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestDeal(t *testing.T){
+	d := newDeck()
+	hand, remainingDeck := deal(d, 5)
+
+	if len(hand) != 5{
+		t.Errorf("Expected hand length of 5, but got %v", len(hand))
+	}
+	if len(remainingDeck) != 47 {
+		t.Errorf("Expected remaining deck length of 47, but got %v", len(remainingDeck))
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
--- course/02_cards/main.go
+++ course/02_cards/main.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
-	hand, remainingDeck := deal(cards, 15)
+	// cards := newDeck()
+	// hand, remainingDeck := deal(cards, 15)
 
+	// cards.print()
+	// fmt.Println("----------")
+	// hand.print()
+	// fmt.Println("----------")
+	// remainingDeck.print()
+
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+	// fmt.Println(cards.toString())
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	cards.print()
-	fmt.Println("----------")
-	hand.print()
-	fmt.Println("----------")
-	remainingDeck.print()
 }
